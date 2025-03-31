a173511fbdc9108e2a8ff77119f26615b246243b
mightbegood12 <banunaresh1976@gmail.com>
2025-03-31 23:22:08 +0530

    [FIX] Agent finetuning, UI/UX

--- showgo-app/src/pages/home.tsx
+++ showgo-app/src/pages/home.tsx
@@ -9,7 +9,9 @@ export default function Home() {
   >([]);
   const [input, setInput] = useState("");
   const [messageSent, setMessageSent] = useState(false);
+  const [canBook, setCanBook] = useState(false);
   const messagesEndRef = useRef<HTMLDivElement>(null);
+  const inputRef = useRef<HTMLInputElement>(null);
 
   const scrollToBottom = () => {
     messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
@@ -22,6 +24,7 @@ export default function Home() {
 
   useEffect(() => {
     scrollToBottom();
+    inputRef.current?.focus();
   }, [messages]);
 
   const defaultResponse = async (input: string) => {
@@ -32,7 +35,7 @@ export default function Home() {
         {
           role: "system",
           content:
-            "You are CineRush AI agent, a Named Entity Recognition (NER) model specialized in movie ticket booking. Your task is to extract key details such as movie name, location, time, preferences, and date from the user's input and return the information in JSON format. If any required details are missing, prompt the user to provide them. Always include a reply_msg field with a natural response guiding the user through the booking process. Do not explicitly mention that you are extracting information. Once all necessary details are gathered, confirm with the user and indicate that you are proceeding with the booking. Keep your responses strictly related to movie ticket booking and do not provide assistance beyond this scope.",
+            "You are CineRush AI agent, a Named Entity Recognition (NER) model specialized in movie ticket booking. Your task is to extract key details such as movie name, location, time, preferences, and date from the user's input and return the information in JSON format. Also when you have every necessary entities, add a boolean entity 'can_book' to the JSON output. If any required details are missing, prompt the user to provide them. Always include a reply_msg field with a natural response guiding the user through the booking process. Do not explicitly mention that you are extracting information. Once all necessary details are gathered, confirm with the user and indicate that you are proceeding with the booking. Keep your responses strictly related to movie ticket booking and do not provide assistance beyond this scope.",
         },
         {
           role: "user",
@@ -54,6 +57,7 @@ export default function Home() {
       { role: "assistant", content: jsonResponse.reply_msg || "" },
     ]);
     console.log(response.choices[0].message.content);
+    setCanBook(jsonResponse.can_book);
   };
 
   const handleSubmit = (e: FormEvent) => {
@@ -72,23 +76,40 @@ export default function Home() {
 
   return (
     <main className="flex flex-col h-screen bg-[#212121]">
-      {/* Landing Page */}
-      {!messageSent && (
-        <div className="flex flex-col text-white justify-center h-full items-center p-4">
-          <div className="font-bold text-4xl">CineRush AI Agent</div>
-          <div className="text-sm font-light">
-            Your goto place for booking tickets
-          </div>
+      {/* Logo Header */}
+      <motion.div
+        initial={{ opacity: 0, y: 10 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.5 }}
+        className={`flex flex-col text-white justify-center ${
+          messageSent ? "h-auto" : "h-[80%]"
+        } items-center p-4`}
+      >
+        <div
+          className={`font-bold text-center transition-all duration-300 ${
+            messageSent ? "text-2xl" : "text-6xl"
+          } `}
+        >
+          CineRush AI Agent
         </div>
-      )}
+        <div
+          className={`"text-sm text-center font-light ${
+            messageSent ? "hidden" : "block"
+          } `}
+        >
+          Your goto place for booking tickets
+        </div>
+      </motion.div>
       {/* Chat container + */}
-      <div className="flex flex-row gap-4 h-[90%] w-full">
-        <div className="flex-1 overflow-y-auto p-4 mt-12">
+      <motion.div
+        initial={{ opacity: 0, y: 10 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.5 }}
+        className="flex flex-row gap-4 h-[80%] w-full"
+      >
+        <div className="flex-1 overflow-y-auto p-4 ">
           <div
-            className={`${
-              messageSent ? "max-w-2xl ml-2" : "max-w-3xl mx-auto"
-            }`}
-            // className="max-w-3xl mx-auto"
+            className={`${canBook ? "max-w-2xl ml-2" : "max-w-3xl mx-auto"}`}
           >
             {messages.map((message, index) => (
               <div
@@ -115,7 +136,7 @@ export default function Home() {
             <div ref={messagesEndRef} />
           </div>
         </div>
-        {messageSent && (
+        {canBook && (
           <motion.div
             initial={{ opacity: 0, x: 100 }}
             animate={{ opacity: 1, x: 0 }}
@@ -130,16 +151,17 @@ export default function Home() {
             ></iframe>
           </motion.div>
         )}
-      </div>
+      </motion.div>
       {/* Input form */}
-      <div className="border-t border-white/[0.1] p-4">
+      <div className="border-t absolute bottom-0 w-full z-20 bg-[#212121] border-white/[0.1] p-4">
         <form onSubmit={handleSubmit} className="max-w-3xl mx-auto">
           <div className="flex items-center space-x-2 text-white">
             <input
+              ref={inputRef}
               type="text"
               value={input}
               onChange={(e) => setInput(e.target.value)}
-              placeholder="Book me a ticket for The Batman movie this weekend in PVR, chennai at 10:00 AM"
+              placeholder="Book me a ticket for The Batman movie this weekend in PVR, chennai at 10:00 AM tomorrow."
               className="flex-1 shadow-md border-[1px] border-white/[0.1] px-4 py-3 rounded-xl bg-[#303030] focus:outline-none"
             />
             <button
