91a4d7a9f5a68bbbc53547878a1221b4bc5c8c6a
mightbegood12 <banunaresh1976@gmail.com>
2025-05-30 22:29:58 +0530

    [FIX] Vercel Build Config

--- cineRush-backend/server.js
+++ cineRush-backend/server.js
@@ -74,3 +74,7 @@ app.use("/api/bookticket", bookingRouter);
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+
+app.get("/", (req, res) => {
+  res.send("Working");
+});
